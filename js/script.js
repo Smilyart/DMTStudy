ea0426c627e43fd48a838dfee6069ffbc8a95ce4
Smilyart <voverdoterskij@gmail.com>
2023-05-16 23:01:34 +0300

    Update 1.4

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.feedback__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinity: true,
+      autoplay: true,
+      autoplaySpeed: 7000
+    });
+});
