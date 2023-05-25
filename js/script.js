dcea95df02cdf8f6f9b915018500d55b28913c97
Smilyart <voverdoterskij@gmail.com>
2023-05-25 08:20:05 +0300

    Update 1.8: 8 blocks adaptate

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
       autoplaySpeed: 7000
     });
 });
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".nav");
+const menuItem = document.querySelectorAll(".burger__item");
+
+burger.addEventListener("click", function() {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+for (let i = 0; i < menuItem.length; i++) {
+  menuItem[i].addEventListener("click", function() {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
+}
